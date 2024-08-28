8075875afcf4865f7e4fdcb3756281c91bbd8fb6
Valentin Hervieu <valentin@hervi.eu>
2024-08-28 14:17:33 +0200

    test with webhook.site
    the goal is to know what headers are added by Vercel

--- src/request.ts
+++ src/request.ts
@@ -1,7 +1,7 @@
 const BASE_URL = process.env.BASE_URL;
 
 export const testRequest = async () => {
-  const response = await fetch(`${BASE_URL}/api/groups/`, {
+  const response = await fetch('https://webhook.site/eedcbf4f-66da-4baa-a725-df6c3ab64292', {
     method: 'POST',
     body: '{}',
     headers: {
