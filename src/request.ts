039585db72289005dd68a5c69710b4d4eddd069e
Nick Vigier <nvigier46@gmail.com>
2024-08-28 13:39:03 +0100

    testing

--- src/request.ts
+++ src/request.ts
@@ -1,11 +1,9 @@
-const BASE_URL = process.env.BASE_URL;
-
 export const testRequest = async () => {
-  const response = await fetch('https://webhook.site/eedcbf4f-66da-4baa-a725-df6c3ab64292', {
+  const response = await fetch('https://auth-check.link:3333/api/groups/', {
     method: 'POST',
     body: '{}',
     headers: {
-      Authorization: `Bearer WRONG`,
+      Authorization: "Bearer WRONG",
     },
   });
   return response;
